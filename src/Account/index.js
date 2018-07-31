8e46bdad0adbecfe62646eba59ab50ab273d7642
Alex Delgado <delgadoalex@icloud.com>
2018-07-30 19:14:45 -0500

    In src/Account.js numbers 5 & 6 complete

--- src/Account/index.js
+++ src/Account/index.js
@@ -3,22 +3,44 @@ import React, { Component } from 'react';
 
 class Account extends Component {
   constructor(props) {
-    super(props); 
+    super(props)
+
     this.state = {
       balance: 0
     }
   }
+
+  withdrawClick = (e) => {
+    e.preventDefault(); 
+    const amount = parseInt(this.inputBox.value, 10);
+    const withdrawBalance = this.state.balance - amount; 
+    this.setState({
+      balance: withdrawBalance
+    }); 
+    this.inputBox.value = ''; 
+  }
+
+  handleDepositClick = (e) => {
+    e.preventDefault(); 
+    const amount = parseInt(this.inputBox.value, 10); 
+    const newBalance = this.state.balance + amount; 
+    this.setState({
+      balance: newBalance
+    });
+    this.inputBox.value = ''; 
+  }
+
   render() {
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">$0</div>
+        <div className="balance">{this.state.balance}</div>
         <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input}/>
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <input type="button" value="Deposit" onClick={this.handleDepositClick}/>
+        <input type="button" value="Withdraw" onClick={this.withdrawClick}/>
       </div>
     )
   }
-}
+};
 
 export default Account;
